19b280369ad0fa08b7ebfadc515cf2f33524332c
Robyn Jackey <15696076+rjackey@users.noreply.github.com>
2020-12-21 09:51:11 -0500

    add project startup/shutdown scripts - now it temporarily disables an installed copy when the MATLAB project is open

---
+++ runProjectStartup.m
@@ -0,0 +1,25 @@
+% Run project startup tasks
+
+% Copyright 2019-2020 The MathWorks, Inc.
+
+
+%% Close any open components first
+
+close all
+warning('off','MATLAB:ClassInstanceExists');
+clear classes %#ok<CLCLS>
+warning('on','MATLAB:ClassInstanceExists');
+
+
+%% Disable any installed version
+
+% Get installed addons
+addonInfo = matlab.addons.installedAddons();
+
+% Addon ID
+addonId = "78895307-cc36-4970-8b66-0697da8f9352"; % Widgets Toolbox 2.x
+
+% Disable
+if ismember(addonId, addonInfo.Identifier)
+    matlab.addons.disableAddon(addonId);
+end
