b7d941814258a3a783867ed3a73df1602295afd8
rjackey <rjackey@MATHWORKS-hZA1M.dhcp.mathworks.com>
2023-03-06 11:19:59 -0500

    Add timezone and NaT support to DateTimePicker #14

--- widgets/+wt/DatetimeSelector.m
+++ widgets/+wt/DatetimeSelector.m
@@ -5,7 +5,7 @@ classdef DatetimeSelector < matlab.ui.componentcontainer.ComponentContainer & ..
 
     % Date and time selection control
 
-    % Copyright 2020-2022 The MathWorks Inc.
+    % Copyright 2020-2023 The MathWorks Inc.
 
 
     %% Events
@@ -18,16 +18,26 @@ classdef DatetimeSelector < matlab.ui.componentcontainer.ComponentContainer & ..
 
 
     %% Public properties
-    properties (AbortSet)
+    properties
 
         % The current value shown
         Value (1,1) datetime
+
+    end %properties
+
+
+    properties (AbortSet)
 
         % The time format
         ShowAMPM (1,1) matlab.lang.OnOffSwitchState = 'on'
 
         % Show seconds or not
         ShowSeconds (1,1) matlab.lang.OnOffSwitchState = 'off'
+
+        % Show timezone
+        ShowTimeZone (1,1) matlab.lang.OnOffSwitchState = 'off'
+        %         ShowTimeZone (1,1) wt.enum.TimeZoneDisplayState = ...
+        %             wt.enum.TimeZoneDisplayState.none
 
     end %properties
 
@@ -51,8 +61,7 @@ classdef DatetimeSelector < matlab.ui.componentcontainer.ComponentContainer & ..
 
 
     %% Internal Properties
-    properties ( Transient, NonCopyable, ...
-            Access = {?matlab.uitest.TestCase, ?matlab.ui.componentcontainer.ComponentContainer} )
+    properties (Transient, NonCopyable, Hidden, SetAccess = protected)
 
         % Button
         DateControl (1,1) matlab.ui.control.DatePicker
@@ -71,6 +80,17 @@ classdef DatetimeSelector < matlab.ui.componentcontainer.ComponentContainer & ..
 
         % AmPm control
         AmPmControl (1,1) matlab.ui.control.DropDown
+
+        % TimeZone control
+        TimeZoneControl (1,1) matlab.ui.control.DropDown
+
+    end %properties
+
+
+    properties (Constant, Access = protected)
+
+        % Time zone info
+        TimeZoneInfo table = getTimeZoneInfo();
 
     end %properties
 
@@ -98,7 +118,7 @@ classdef DatetimeSelector < matlab.ui.componentcontainer.ComponentContainer & ..
             obj.Grid.Padding = 0;
 
             % Configure Grid
-            obj.Grid.ColumnWidth = {'9x',5,'4x','4x',0,0};
+            obj.Grid.ColumnWidth = {'9x',5,'4x','4x',0,0,0};
             obj.Grid.RowHeight = {'1x'};
             obj.Grid.ColumnSpacing = 0;
 
@@ -139,6 +159,9 @@ classdef DatetimeSelector < matlab.ui.componentcontainer.ComponentContainer & ..
                 "Items",["AM","PM"],...
                 "ValueChangedFcn",@(h,e)obj.onTimeEdited(e));
 
+            obj.TimeZoneControl = uidropdown(obj.Grid);
+            obj.TimeZoneControl.ValueChangedFcn = @(h,e)obj.onTimeZoneChanged(e);
+
             % Update the internal component lists
             allFields = [
                 obj.DateControl
@@ -146,6 +169,7 @@ classdef DatetimeSelector < matlab.ui.componentcontainer.ComponentContainer & ..
                 obj.MinuteControl
                 obj.SecondControl
                 obj.AmPmControl
+                obj.TimeZoneControl
                 ];
             obj.FontStyledComponents = allFields;
             obj.FieldColorableComponents = allFields;
@@ -160,20 +184,27 @@ classdef DatetimeSelector < matlab.ui.componentcontainer.ComponentContainer & ..
             % Get the value
             v = obj.Value;
 
-            %RAJ - handle NaT
-
             % Toggle visibilities
             obj.SecondControl.Visible = obj.ShowSeconds;
             obj.AmPmControl.Visible = obj.ShowAMPM;
+            obj.TimeZoneControl.Visible = obj.ShowTimeZone;
+
             if obj.ShowSeconds
                 obj.Grid.ColumnWidth{5} = '4x';
             else
                 obj.Grid.ColumnWidth{5} = 0;
             end
+
             if obj.ShowAMPM
                 obj.Grid.ColumnWidth{6} = '5x';
             else
                 obj.Grid.ColumnWidth{6} = 0;
+            end
+
+            if obj.ShowTimeZone
+                obj.Grid.ColumnWidth{7} = '8x';
+            else
+                obj.Grid.ColumnWidth{7} = 0;
             end
 
             % Update the date control
@@ -185,16 +216,18 @@ classdef DatetimeSelector < matlab.ui.componentcontainer.ComponentContainer & ..
             if obj.ShowAMPM
 
                 % Update the AM/PM
-                if v.Hour < 12
-                    obj.AmPmControl.Value = "AM";
-                else
+                if v.Hour >= 12
                     obj.AmPmControl.Value = "PM";
+                else
+                    obj.AmPmControl.Value = "AM";
                 end
 
                 % Update the hour control
                 if v.Hour > 12
                     obj.HourControl.Value = v.Hour - 12;
                 elseif v.Hour == 0
+                    obj.HourControl.Value = 12;
+                elseif isnan(v.Hour)
                     obj.HourControl.Value = 12;
                 else
                     obj.HourControl.Value = v.Hour;
@@ -207,13 +240,66 @@ classdef DatetimeSelector < matlab.ui.componentcontainer.ComponentContainer & ..
                 % Update the hour control
                 %obj.HourControl.Limits = [0 23];
                 obj.HourControl.ValueDisplayFormat = "%02.0f";
-                obj.HourControl.Value = v.Hour;
+                if isnan(v.Hour)
+                    obj.HourControl.Value = 0;
+                else
+                    obj.HourControl.Value = v.Hour;
+                end
 
             end %if obj.ShowAMPM
 
-            % Update the minutes and seconds
-            obj.MinuteControl.Value = v.Minute;
-            obj.SecondControl.Value = v.Second;
+            % Update the minutes
+            if isnan(v.Minute)
+                obj.MinuteControl.Value = 0;
+            else
+                obj.MinuteControl.Value = v.Minute;
+            end
+
+            % Update the seconds
+            if isnan(v.Second)
+                obj.SecondControl.Value = 0;
+            else
+                obj.SecondControl.Value = v.Second;
+            end
+
+            % Prepare time zone list and value
+            if obj.ShowTimeZone
+
+                % Get the current value
+                tzValue = obj.Value.TimeZone;
+
+                % Prepare the list selections
+                if startsWith(tzValue,["+","-"])
+                    % Current timezone is just a value with no region!
+
+                    allOffsets = unique(obj.TimeZoneInfo.OffsetName,'stable');
+
+                    value = tzoffset(obj.Value);
+                    valueStr = string( value );
+                    if value >= 0
+                        valueStr = "+" + valueStr;
+                    end
+
+                    tzItems = allOffsets;
+                    tzItemsData = allOffsets;
+                    tzValue = valueStr;
+
+                else
+                    % Current timezone is a standard region name
+                    % Or it is an empty string
+
+                    tzItems = obj.TimeZoneInfo.CombinedName;
+                    tzItemsData = obj.TimeZoneInfo.Name;
+                    tzValue = obj.Value.TimeZone;
+
+                end
+
+                % Update time zone
+                obj.TimeZoneControl.Items = tzItems;
+                obj.TimeZoneControl.ItemsData = tzItemsData;
+                obj.TimeZoneControl.Value = tzValue;
+
+            end %if obj.ShowTimeZone
 
         end %function
 
@@ -233,15 +319,24 @@ classdef DatetimeSelector < matlab.ui.componentcontainer.ComponentContainer & ..
             % Get the new date
             newValue = evt.Source.Value;
 
-            % Update the value
-            value = obj.Value;
-            value.Year = newValue.Year;
-            value.Month = newValue.Month;
-            value.Day = newValue.Day;
-            obj.Value = value;
+            % Existing date (if missing, use today)
+            dt = obj.Value;
+            if ismissing(dt)
+                dt = datetime("today",...
+                "TimeZone","local",...
+                "Format","dd-MMM-uuuu hh:mm aa");
+            end
+
+            % Set the date
+            dt.Year = newValue.Year;
+            dt.Month = newValue.Month;
+            dt.Day = newValue.Day;
+
+            % Update value
+            obj.Value = dt;
 
             % Trigger event
-            evtOut = wt.eventdata.ValueChangedData(obj.Value);
+            evtOut = wt.eventdata.ValueChangedData(dt);
             notify(obj,"ValueChanged",evtOut);
 
         end %function
@@ -252,7 +347,14 @@ classdef DatetimeSelector < matlab.ui.componentcontainer.ComponentContainer & ..
 
             % Get the new value
             newValue = evt.Source.Value;
+
+            % Existing date (if missing, use today)
             dt = obj.Value;
+            if ismissing(dt)
+                dt = datetime("today",...
+                "TimeZone","local",...
+                "Format","dd-MMM-uuuu hh:mm aa");
+            end
 
             % What was changed?
             switch evt.Source
@@ -281,6 +383,24 @@ classdef DatetimeSelector < matlab.ui.componentcontainer.ComponentContainer & ..
                     end
 
             end %switch
+
+            % Update value
+            obj.Value = dt;
+
+            % Trigger event
+            evtOut = wt.eventdata.ValueChangedData(obj.Value);
+            notify(obj,"ValueChanged",evtOut);
+
+        end %function
+
+
+        function onTimeZoneChanged(obj,evt)
+            % Triggered on edits
+
+            % Get the new value
+            newValue = evt.Value;
+            dt = obj.Value;
+            dt.TimeZone = newValue;
 
             % Update value
             obj.Value = dt;
@@ -329,3 +449,33 @@ classdef DatetimeSelector < matlab.ui.componentcontainer.ComponentContainer & ..
     end %methods
 
 end % classdef
+
+
+%% Helper Functions
+function timeZoneInfo = getTimeZoneInfo()
+% Format a table of the necessary time zone info
+
+% Get the timezones
+timeZoneInfo = timezones();
+timeZoneInfo.Name = string(timeZoneInfo.Name);
+
+% Remove the Etc timezones
+% These seem to have UTCOffset that is negative of what the name implies,
+% causing issues
+timeZoneInfo(timeZoneInfo.Area == "Etc", :) = [];
+
+% Sort by offset from GMT
+timeZoneInfo = sortrows(timeZoneInfo, "UTCOffset");
+
+% Prepare the formatted offsets
+offsetH = fix(timeZoneInfo.UTCOffset);
+offsetM = mod(timeZoneInfo.UTCOffset, 1) * 60;
+
+% Attach items to the table
+timeZoneInfo.OffsetName = compose("%+03d:%02d", offsetH, offsetM);
+timeZoneInfo.CombinedName = timeZoneInfo.OffsetName + "  " + timeZoneInfo.Name;
+
+% Add a blank row to the top for empty timezone info
+timeZoneInfo = vertcat({"","",NaN,NaN,"",""}, timeZoneInfo);
+
+end %function
