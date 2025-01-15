1fac9a31ab671448ae50490f3387d51af137da4d
Robyn Jackey <rjackey@mathworks.com>
2025-01-14 19:46:58 -0500

    work on packaging code

---
+++ deploy/+wt/+deploy/readVersionNumber.m
@@ -0,0 +1,20 @@
+function [versionStr, versionParts, versionFile] = readVersionNumber()
+% Retrieves the version number for PDT
+
+% Version file path
+thisPath = mfilename("fullpath");
+sourceFolder = fileparts(fileparts(fileparts(thisPath)));
+versionFile = fullfile(sourceFolder,"wtDeployVersion.txt");
+
+% Read in the version parts
+if isfile(versionFile)
+    versionParts = readmatrix(versionFile,...
+        "Delimiter",".",...
+        "OutputType","uint32");
+else
+    % Unknown Version
+    versionParts = [0 0 0];
+end
+
+% Format as string
+versionStr = join(string(versionParts),".");
---
+++ deploy/packageRelease.m
@@ -0,0 +1,44 @@
+function outputFile = packageRelease()
+% Package a Toolbox Release
+
+% Copyright 2025 The MathWorks, Inc.
+
+
+%% Get paths
+
+% Project root
+proj = currentProject;
+projectRoot = proj.RootFolder;
+
+
+%% Run unit tests
+testResult = runTestSuite;
+if ~all([testResult.Passed])
+    error("Unit tests failed. Aborting package release.");
+end
+
+
+%% Increment Version Number
+
+% Increment the last part of the version number in wtDeployVersion.txt
+% (Other changes require commenting this out and making manual edits)
+wt.deploy.incrementVersionNumber();
+
+
+%% Package the Release
+
+% Increment the last part of the version number in version.txt
+toolboxVersion = wt.deploy.readVersionNumber();
+
+% Read in the package options
+opts = wt.deploy.getPackageOptions(projectRoot, toolboxVersion);
+outputFile = opts.OutputFile;
+
+% Perform the packaging
+matlab.addons.toolbox.packageToolbox(opts);
+
+
+%% Open the release folder
+
+releaseFolder = fullfile(projectRoot,"release");
+winopen(releaseFolder);
