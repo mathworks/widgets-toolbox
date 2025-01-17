c1974ea4102a4bcfcf20901bee1a527853d8c120
Robyn Jackey <rjackey@mathworks.com>
2025-01-16 21:53:31 -0500

    work on documentation

--- deploy/wtPackageRelease.m
+++ deploy/wtPackageRelease.m
@@ -9,12 +9,53 @@
 proj = currentProject;
 projectRoot = proj.RootFolder;
 
+docInputPath = fullfile(projectRoot,"doc_input");
+docOutputPath = fullfile(projectRoot,"widgets","doc");
+
+examplesInputPath = fullfile(projectRoot,"widgets","examples");
+examplesOutputPath = fullfile(projectRoot,"widgets","examples","html");
+
+gettingStartedInputPath = fullfile(projectRoot,"widgets","doc","GettingStarted.mlx");
+gettingStartedOutputPath = fullfile(projectRoot,"widgets","doc");
+
 
 %% Run unit tests
 % [testSuite, testResult]  = runTestSuite;
 % if ~all([testResult.Passed])
 %     error("Unit tests failed. Aborting package release.");
 % end
+
+
+%% Publish doc_input as html
+
+% Find the doc input ".mlx" files
+docInputInfo = what(docInputPath);
+docInputFiles = docInputInfo.mlx;
+docInputFiles = fullfile(docInputPath, docInputFiles);
+
+% Publish these to HTML output
+wt.deploy.publishLiveScriptToHtml(docInputFiles, docOutputPath)
+
+
+%% Publish examples as html
+
+% Find the examples ".mlx" files
+examplesInputInfo = what(examplesInputPath);
+examplesInputFiles = examplesInputInfo.mlx;
+examplesInputFiles = fullfile(examplesInputPath, examplesInputFiles);
+
+% Publish these to HTML output
+wt.deploy.publishLiveScriptToHtml(examplesInputFiles, examplesOutputPath)
+
+
+%% Publish GettingStarted.mlx as html
+
+% Publish to HTML output
+wt.deploy.publishLiveScriptToHtml(gettingStartedInputPath, gettingStartedOutputPath)
+
+
+%% Build search database
+builddocsearchdb(docOutputPath)
 
 
 %% Increment Version Number
