93df63e6b371b1f6aed41c8e759f1cdf43a7c9c8
Jorik Caljouw <jorik@versionbay.com>
2025-02-24 15:24:05 +0100

    add ProjectIntegrityChecks test class

---
+++ test/+wt/+test/ProjectIntegrityChecks.m
@@ -0,0 +1,30 @@
+classdef ProjectIntegrityChecks < matlab.unittest.TestCase
+    % Implements a unit test that runs the Project Integrity Checks
+
+    properties(Access = private)
+        DoNotRun = verLessThan("MATLAB", "9.9"); %#ok<VERLESSMATLAB> to support running this in < R2020b
+    end
+
+    methods(Test)
+        function runProjectChecks(testCase)
+            % do not continue if DoNotRun is set
+            testCase.assumeFalse(testCase.DoNotRun, "Integrity Checks can only be run on R2020b or newer programmatically.")
+
+            % find project root folder based on this file (3 folders up)
+            prjFolder = fileparts(fileparts(fileparts(fileparts(mfilename("fullpath")))));
+            testCase.applyFixture(matlab.unittest.fixtures.ProjectFixture(prjFolder))
+
+            proj = currentProject();
+
+            checkResults = runChecks(proj);
+
+            % Verify checks with useful diagnostic
+            for idx = 1:length(checkResults)
+                diagnostic = "Failed Integrity Test: '" + checkResults(idx).Description + "'" + newline + "Problem files:" + newline + sprintf("%s\n", checkResults(idx).ProblemFiles);
+                testCase.verifyTrue(checkResults(idx).Passed, diagnostic);
+            end
+
+        end
+    end
+
+end
